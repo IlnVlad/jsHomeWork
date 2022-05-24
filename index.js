9f4ad660770b2dfb2c1d626f64868ac7d8d4f490
Владислав <saxarov2010@gmail.com>
2022-05-24 16:33:57 +1000

    Выполнил ДЗ 2.

--- index.js
+++ index.js
@@ -7,16 +7,40 @@ const goods = [
     { title: 'Shoes', price: 250 },
 ];
 
-const renderGoodsItem = (title = 'значение', price = 'значение') => {
-    return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
-};
+class GoodsItem {
+    constructor({ title = 'значение', price = 'значение' }) {
+        this.title = title;
+        this.price = price;
 
-const renderGoodsList = (list =[]) => {
-    let goodsList = list.map((item) => renderGoodsItem(item.title, item.price));
-    document.querySelector('.goods-list').innerHTML = goodsList.join(' ');
+    }
+    render() {
+        return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
+    }
 }
 
-renderGoodsList(goods);
 
-//3*
-// так как результат map заменяет собой элементы объекта, в котором был вызван, то в объекте goodList разделитель ",". А innerHTML вставит весь объект, в том числе и разделитель. Разделитель можно заменить с помощью goodsList.join(" ")
+class GoodsList {
+    constructor(list = []) {
+        this.list = list;
+    }
+    render() {
+        const goodsList = this.list.map((item) => {
+            const goodsItem = new GoodsItem(item);
+            return goodsItem.render();
+        });
+        document.querySelector('.goods-list').innerHTML = goodsList.join(' ');
+    }
+    getPrice() {
+        const allPrice = this.list.reduce((accumulator, currentValue) => {
+            return accumulator + currentValue.price;
+        }, 0);
+        document.querySelector('.goods-list-allPrice').innerHTML = `Цена всех товаров: ${allPrice}ед.`;
+    }
+
+}
+
+
+
+const goodsList = new GoodsList(goods);
+goodsList.render();
+goodsList.getPrice();
