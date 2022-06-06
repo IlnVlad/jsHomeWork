1fd98f8955bbe1990647be480907b8d7e5fe212f
IlnVlad <95472027+IlnVlad@users.noreply.github.com>
2022-06-06 14:14:00 +1000

    Выполнил ДЗ-5
    Выполнил ДЗ-5

--- index.js
+++ index.js
@@ -1,78 +1,31 @@
-"use strict";
-
-const url = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/catalogData.json';
-const basketUrl = `https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/getBasket.json`;
-
-class GoodsItem {
-    constructor({ product_name = 'значение', price = 'значение' }) {
-        this.product_name = product_name;
-        this.price = price;
-
-    }
-    render() {
-        return `<div class="goods-item"><h3>${this.product_name}</h3><p>${this.price}</p></div>`;
-    }
+window.onload = () => {
+    const app = new Vue({
+        el: "#root",
+        data: {
+            empty: 'Нет данных',
+            items: [],
+            searchValue: '',
+            isVisible: false
+        },
+        mounted() {
+            fetch('https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/catalogData.json').then(res => res.json()).then(
+                data => {
+                    this.items = data;
+                    this.empty = '';
+                }
+            )
+        },
+        computed: {
+            filteredItems() {
+                return this.items.filter(({ product_name }) => {
+                    return product_name.match(new RegExp(this.searchValue, 'gui'))
+                })
+            }
+        },
+        methods: {
+            cartButtonClick() {
+                this.isVisible = !this.isVisible
+            }
+        }
+    })
 }
-
-
-class GoodsList {
-    constructor(list = []) {
-        this.list = this.getData();
-        this.filteredItems = this.getData();
-    }
-    async getData() {
-        const response = await fetch(url);
-        const data = await response.json();
-        return (this.list = await data, this.filteredItems = data);
-    }
-    async filter(str) {
-        await this.filteredItems;
-        this.filteredItems = this.list.filter(({ product_name }) => {
-            return (new RegExp(str, 'i')).test(product_name)
-        });
-    }
-    async render() {
-        await this.filteredItems;
-        const goodsList = await this.filteredItems.map((item) => {
-            const goodsItem = new GoodsItem(item);
-            return goodsItem.render();
-        });
-        document.querySelector('.goods-list').innerHTML = goodsList.join(' ');
-    }
-    async getPrice() {
-        await this.list;
-        const allPrice = this.list.reduce((accumulator, currentValue) => {
-            return accumulator + currentValue.price;
-        }, 0);
-        document.querySelector('.goods-list-allPrice').innerHTML = `Цена всех товаров: ${allPrice}ед.`;
-    }
-
-}
-
-class BasketItems {
-    constructor(items = []) {
-        this.items = this.getBasket();
-    }
-    async getBasket() {
-        const response = await fetch(basketUrl);
-        const data = await response.json();
-        return this.items = await data.contents;
-    }
-}
-
-
-const goodsList = new GoodsList();
-goodsList.render();
-goodsList.getPrice();
-
-const basket = new BasketItems();
-basket.getBasket();
-console.log(basket);
-
-document.querySelector('.search-button').addEventListener('click', () => {
-    const input = document.querySelector('.search');
-    goodsList.filter(input.value);
-    goodsList.render();
-});
-
-
